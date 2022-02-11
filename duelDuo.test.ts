3f83946116c254b6e4f40b008e72cec848a3a5f6
Benberto <ben.bertagnole@gmail.com>
2022-02-11 11:23:06 -0700

    Fixed testing

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -12,31 +12,36 @@ beforeEach(async () => {
 afterAll(async () => {
     driver.quit()
 })
-
+describe('Test Duel Duo', () => {
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
-})
+});
 
 test('Draw button should display', async () => {
     const button = await driver.findElement(By.id('draw'))
     await button.click()
+    await driver.sleep(3000)
     const bots = await driver.findElement(By.css('div#choices.container'))
     const displayBots = await bots.isDisplayed()
     expect(displayBots).toBe(true)
-   
-})
+
+});
 
 
 test("Add to Duo button adds bot to duo", async () => {
     const drawButton = await driver.findElement(By.id("draw"));
     await drawButton.click();
-    const divs = await driver.findElement(By.id("choices"));
+    await driver.sleep(3000)
+    const bots = await driver.findElement(By.css('div#choices.container'))
+    const displayBots = await bots.isDisplayed()
     const botBtn = await driver.findElement(By.className("bot-btn"));
     await botBtn.click();
+    await driver.sleep(3000)
     const playerBot = await driver.findElement(By.id("player-duo"));
     const isDisplayed = await playerBot.isDisplayed();
     expect(isDisplayed).toBe(true);
-    
-  });
+});
+
+});
